9021b919bde378e911ef9cb89bdaea3a723f7ee6
Sean Forbes <seanpchforbes@alibinexus.net>
2025-10-29 16:30:30 +1000

    .

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import pygame
+from constants import *
+
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # GAME LOOP
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(0x000000)
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
